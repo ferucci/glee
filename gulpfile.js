6b666397447f377b6ede5dd0e56b8cfb8dee1a74
ferucci <development.first1@gmail.com>
2022-01-03 20:37:40 +0300

    experimental project modularity,

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 // Присваиваю возможности Gulp-a
 const { src, dest, watch, parallel, series } = require('gulp');
-const scss         = require('gulp-sass')(require('sass'));
-const concat       = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify       = require('gulp-uglify');
-const imagemin     = require('gulp-imagemin');
-const del          = require('del');
-const browserSync  = require('browser-sync').create();
+const scss              = require('gulp-sass')(require('sass'));
+const concat            = require('gulp-concat');
+const autoprefixer      = require('gulp-autoprefixer');
+const uglify            = require('gulp-uglify');
+const nunjucksRender    = require('gulp-nunjucks-render');
+const imagemin          = require('gulp-imagemin');
+const rename            = require('gulp-rename');
+const del               = require('del');
+const browserSync       = require('browser-sync').create();
 
 //Создаёмфункцию для работы плагина browser-sync (вся информация есть в документации)
 function browsersync() {
@@ -19,11 +21,21 @@ function browsersync() {
     });
 }
 
+function nunjuksR(){
+    return src('app/*.njk')
+    .pipe(nunjucksRender())
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+}
+
 // Конвертируем scss в css
 function styles() {
-    return src('app/scss/style.scss')
+    return src('app/scss/*.scss')
         .pipe(scss({ outputStyle: 'compressed' }))
-        .pipe(concat('style.min.css'))
+        // .pipe(concat())
+        .pipe(rename({
+            suffix: '.min'
+        }))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 10 versions'],
             grid: true
@@ -87,6 +99,7 @@ function cleanDist(){
 // Функция watching "методом" watch заходит в папку app/scss/находит здесь все папки/и все файлы с разрешением .scss , далее следит за изменениями styles
 function watching() {
     watch(['app/scss/**/*.scss'], styles);
+    watch(['app/*.njk'], nunjuksR);
     // '!app/js/main.min.js' следить за всеми файлами,КРОМЕ(! - оператор HE вроде)
     watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
     // Так же нужно назначить смотрящего за html
@@ -99,11 +112,12 @@ exports.styles = styles;
 exports.scripts = scripts;
 exports.browsersync = browsersync;
 exports.watching = watching;
+exports.nunjuksR = nunjuksR;
 exports.images = images;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, images, build);
 
 
 // Возможность по дефолту. Благодаря свойству parallel можно объединить нужные,они одновременно будут запускаются при запросе gulp
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(nunjuksR, styles, scripts, browsersync, watching);
 
